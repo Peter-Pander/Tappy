00695d72b491c2d1d64ad10aab2e7123b5a1acd8
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-05-30 19:26:19 +0200

    Trigger jump/climb animation to simulate plane lift
    - Uses existing "jump" animation to mimic a plane's upward motion
    - Plays animation on jump input for better visual feedback
    - Keeps internal name "jump" while describing intent more accurately

--- Scenes/Plane/Plane.gd
+++ Scenes/Plane/Plane.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 
 const JUMP_POWER: float = -350.0
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
 var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
 
 # Called when the node enters the scene tree for the first time.
@@ -19,5 +22,6 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("jump") == true:
 		velocity.y = JUMP_POWER
+		animation_player.play("jump")
 
 	move_and_slide()
