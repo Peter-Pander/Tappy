96aa72cae525bd44a6c470dac6c2df2a2cb1698c
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-05-30 15:42:56 +0200

    Add jump input for plane using jump action
    - Plane now responds to "jump" input
    - Applies upward velocity using JUMP_POWER
    - Integrated with existing gravity and move_and_slide()

--- Scenes/Plane/Plane.gd
+++ Scenes/Plane/Plane.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 
+const JUMP_POWER: float = -350.0
 var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
 
 # Called when the node enters the scene tree for the first time.
@@ -16,6 +17,7 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	velocity.y += _gravity * delta
 
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_POWER
+
 	move_and_slide()
-	
-	
