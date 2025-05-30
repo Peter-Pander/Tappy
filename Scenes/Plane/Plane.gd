886300bc8531de2b2184f5251159410f3f83fa55
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-05-30 20:37:58 +0200

    Stop plane animation and physics when hitting the floor
    - Added die() function to stop AnimatedSprite2D animation
    - Disabled further physics processing with set_physics_process(false)
    - Called die() when is_on_floor() is true

--- Scenes/Plane/Plane.gd
+++ Scenes/Plane/Plane.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const JUMP_POWER: float = -350.0
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
@@ -18,10 +19,17 @@ func _process(delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
+	fly(delta)
+	move_and_slide()
+	if is_on_floor() == true:
+		die()
+		
+func fly(delta: float) -> void:
 	velocity.y += _gravity * delta
-
 	if Input.is_action_just_pressed("jump") == true:
 		velocity.y = JUMP_POWER
 		animation_player.play("jump")
 
-	move_and_slide()
+func die() -> void:
+	animated_sprite_2d.stop()
+	set_physics_process(false)
