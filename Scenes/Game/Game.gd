b21b1cb7def0de0e39e2640215320b91b4a8eff6
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-06-01 16:20:09 +0200

    Add return-to-main feature via Escape

--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -2,13 +2,18 @@ extends Node2D
 
 
 const PIPES = preload("res://Scenes/Pipes/Pipes.tscn")
-
+var MAIN = load("res://Scenes/Main/Main.tscn")
 
 @onready var pipes_holder: Node = $PipesHolder
 @onready var upper_point: Marker2D = $UpperPoint
 @onready var lower_point: Marker2D = $LowerPoint
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump") and event.is_echo() == false:
+		get_tree().change_scene_to_packed(MAIN)
+		
+		
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spawn_pipes()
--- Scenes/Main/Main.gd
+++ Scenes/Main/Main.gd
@@ -6,7 +6,6 @@ const GAME = preload("res://Scenes/Game/Game.tscn")
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump") and event.is_echo() == false:
-		print("_unhandled_input:", event)
 		get_tree().change_scene_to_packed(GAME)
 
 # Called when the node enters the scene tree for the first time.
