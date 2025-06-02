dd08d62b2abbcc9e9534ef8d141b7dfea76cbdd3
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-06-03 00:18:47 +0200

    Implement scoring system and connect scoring signal
    - Add `on_point_scored` signal to SignalHub
    - Extend GameUI.gd with `_score` counter and `on_point_scored` method
    - Update score label text dynamically on point score
    - Connect `on_point_scored` signal in `enter_tree()`
    - Add Laser node to Pipes.tscn to act as scoring zone
    - Connect Laser's `body_entered` signal to `_on_laser_body_entered`
    - In `_on_laser_body_entered`, emit score signal and disconnect to prevent duplicates

--- Scenes/GameUI/GameUi.gd
+++ Scenes/GameUI/GameUi.gd
@@ -1,11 +1,20 @@
 extends Control
 
+
+@onready var score_label: Label = $MarginContainer/ScoreLabel
 @onready var game_over_label: Label = $GameOverLabel
 @onready var press_space_label: Label = $PressSpaceLabel
 @onready var timer: Timer = $Timer
 
 
 var _can_press: bool = false
+var _score: int = 0
+
+
+func _ready() -> void:
+	_score = 0
+	
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("Exit"):
@@ -14,13 +23,17 @@ func _unhandled_input(event: InputEvent) -> void:
 		GameManager.load_main_scene()
 
 func _enter_tree() -> void:
-	SignalHub.on_plane_died.connect(on_plane_died)
+	SignalHub.on_point_scored.connect(on_point_scored)
+	SignalHub.on_plane_died.connect(on_plane_died) 
 
 
 func on_plane_died() -> void:
 	game_over_label.show()
 	timer.start()
 
+func on_point_scored() -> void:
+	_score += 1
+	score_label.text = "%04d" % _score
 
 func _on_timer_timeout() -> void:
 	_can_press = true
