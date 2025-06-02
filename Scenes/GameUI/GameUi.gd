42395a7e6911dfa1af34a70df5c050d77a3a487f
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-06-02 22:24:07 +0200

    Implement proper Game Over flow
    - Show 'Game Over' label when plane dies
    - Add Timer node to delay restart availability
    - Reveal 'Press Space' after timer timeout
    - Enable restart via space only after delay (_can_press flag)
    - Update GameUI.gd and GameUI.tscn to support Game Over logic

--- Scenes/GameUI/GameUi.gd
+++ Scenes/GameUI/GameUi.gd
@@ -1,15 +1,28 @@
 extends Control
 
+@onready var game_over_label: Label = $GameOverLabel
+@onready var press_space_label: Label = $PressSpaceLabel
+@onready var timer: Timer = $Timer
+
+
+var _can_press: bool = false
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("Exit") and event.is_echo() == false:
+	if event.is_action_pressed("Exit"):
+		GameManager.load_main_scene()
+	elif _can_press == true and event.is_action_pressed("jump"):
 		GameManager.load_main_scene()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _enter_tree() -> void:
+	SignalHub.on_plane_died.connect(on_plane_died)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func on_plane_died() -> void:
+	game_over_label.show()
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
+	_can_press = true
+	game_over_label.hide()
+	press_space_label.show()
