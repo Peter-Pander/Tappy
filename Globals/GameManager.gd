a81fce521b9de1c66121197814dff83821359d32
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-06-04 18:13:12 +0200

    Add ComplexTransition with animated fade for scene changes
    - Created ComplexTransition scene with fade-to-black animation
    - Added switch_scene logic to ComplexTransition.gd
    - Updated GameManager to preload and use ComplexTransition
    - Replaced direct scene switching with animated transitions

--- Globals/GameManager.gd
+++ Globals/GameManager.gd
@@ -4,15 +4,22 @@ extends Node
 const GAME = preload("res://Scenes/Game/Game.tscn")
 const MAIN = preload("res://Scenes/Main/Main.tscn")
 const SIMPLE_TRANSITION = preload("res://Scenes/SimpleTransition/SimpleTransition.tscn")
+const COMPLEX_TRANSITION = preload("res://Scenes/ComplexTransition/ComplexTransition.tscn")
+
 
 var next_scene = PackedScene
 
+func add_complex() -> void:
+	var ct = COMPLEX_TRANSITION.instantiate()
+	add_child(ct)
 
 
 func load_game_scene() -> void:
 	next_scene = GAME
-	get_tree().change_scene_to_packed(SIMPLE_TRANSITION)
+	add_complex()
+	# get_tree().change_scene_to_packed(SIMPLE_TRANSITION)
 
 func load_main_scene() -> void:
 	next_scene = MAIN
-	get_tree().change_scene_to_packed(SIMPLE_TRANSITION)
+	add_complex()
+	# get_tree().change_scene_to_packed(SIMPLE_TRANSITION)
--- Scenes/SimpleTransition/SimpleTransition.gd
+++ Scenes/SimpleTransition/SimpleTransition.gd
@@ -3,4 +3,5 @@ extends Control
 
 func _ready() -> void:
 	await get_tree().create_timer(1.5).timeout
-	get_tree().change_scene_to_packed(GameManager.next_scene)
+	get_tree().change_scene_to_packed(GameManager.next_scene  )
+   
